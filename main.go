eb11a5289f99ac7750928d0ddb1d489ccfc7d508
Florian Kromer <florian.kromer@mailbox.org>
2019-04-14 16:57:39 +0200

    add detail colelctor to get descriptions, etc.

--- main.go
+++ main.go
@@ -19,15 +19,18 @@ func main() {
 
 	baseUrl := "mathias-kettner.com"
 	url := "https://mathias-kettner.com/cms_check_plugins_list.html"
+
 	collector := colly.NewCollector(
 		colly.AllowedDomains(baseUrl),
 		colly.CacheDir("/tmp/.cpf_cache"), // comment out during debugging
 	)
+	detailCollector := collector.Clone()
 
-	// check plugin list website:
-	// callback triggers on every table line element to get check plugin,
-	// gets the check name from the first td element and the title from the second element.
-	counter := 0
+	counter := 0 // ugly but easiest way to implement the scraper
+
+	// Callback triggers on every table line element on the check plugin overview list.
+	// Check plugin specific sites don't contain the check name which
+	// requires to use this parent collector in addition to the childdetailCollector.
 	collector.OnHTML("tr", func(e *colly.HTMLElement) {
 		e.ForEach("td", func(i int, el *colly.HTMLElement) {
 			switch i {
@@ -38,10 +41,21 @@ func main() {
 				checks = append(checks, check)
 			case 1:
 				checks[counter].Title = el.Text
-				// TODO: trigger child collector
+				el.ForEach("a[href]", func(_ int, ele *colly.HTMLElement) {
+					checks[counter].URL = ele.Request.AbsoluteURL(ele.Attr("href"))
+					// trigger child collector
+					detailCollector.Visit(checks[counter].URL)
+				})
 				counter = counter + 1
 			}
 		})
+	})
+
+	// Callback triggers on every div element which contains check plugin metadata
+	// on check plugin specific sites.
+	detailCollector.OnHTML("div[class=\"nowiki\"]", func(e *colly.HTMLElement) {
+		// TODO: filter to relevant info and assign correspondingly
+		checks[counter].Description = e.Text
 	})
 
 	// debug callbacks
@@ -63,7 +77,9 @@ func main() {
 	collector.Visit(url)
 
 	for _, check := range checks {
-		fmt.Println("Name:   " + check.Name)
-		fmt.Println(" Title: " + check.Title)
+		fmt.Println("Name:         " + check.Name)
+		fmt.Println(" Title:       " + check.Title)
+		fmt.Println(" URL:         " + check.URL)
+		fmt.Println(" Description: " + check.Description)
 	}
 }
