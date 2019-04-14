51ac80800548843c8bc45417e754613c0e8d7fae
Florian Kromer <florian.kromer@mailbox.org>
2019-04-14 17:57:52 +0200

    add command line interface

--- README.md
+++ README.md
@@ -12,7 +12,7 @@ Pipe `cpf` output into `fzf` to fuzzy find in Check_MK plugin meta data.
 
 Download executable into user specific directory for executables and make it executable:
 
-    wget -O ~/.local/bin/cpf ...
+    wget -O ~/.local/bin/cpf https://github.com/fkromer/cpf/releases/tag/0.1.0
     chmod +x ~/.local/bin/cpf
 
 ## Usage
@@ -23,18 +23,20 @@ Download executable into user specific directory for executables and make it exe
 
 Attention: This build procedure is quick and dirty, not reproducible.
 
+    go get gopkg.in/urfave/cli.v1
     go get github.com/gocolly/colly
     go build main.go
     mv main cpf
     chmod +x cpf
 
-## TODO
+## Wishlist
 
 - packaging
-- command line interface with e.g. [cli](https://github.com/urfave/cli) or [cobra](https://github.com/spf13/cobra)
+- refactor command line interface to [cobra](https://github.com/spf13/cobra)
 
 ## Development resources
 
+- [cli (github README)](https://github.com/urfave/cli/blob/master/README.md)
 - [colly (website docs)](http://go-colly.org/docs/)
 - [colly (GoDoc)](https://godoc.org/github.com/gocolly/colly)
 - [Scraping the Web in Golang with Colly and Goquery](https://benjamincongdon.me/blog/2018/03/01/Scraping-the-Web-in-Golang-with-Colly-and-Goquery/)
--- main.go
+++ main.go
@@ -3,6 +3,9 @@ package main
 import (
 	"fmt"
 	"github.com/gocolly/colly"
+	"gopkg.in/urfave/cli.v1"
+	"os"
+	"time"
 )
 
 type Check struct {
@@ -15,71 +18,101 @@ type Check struct {
 }
 
 func main() {
-	checks := make([]Check, 0, 3000) // around 1500 Checks right now
 
-	baseUrl := "mathias-kettner.com"
-	url := "https://mathias-kettner.com/cms_check_plugins_list.html"
+	app := cli.NewApp()
+	app.Name = "cpf"
+	app.Version = "0.1.0"
+	app.Compiled = time.Now()
+	app.Authors = []cli.Author{
+		cli.Author{
+			Name:  "Florian Kromer",
+			Email: "florian.kromer@mailbox.org",
+		},
+	}
+	app.Copyright = "(c) 2019 Florian Kromer"
 
-	collector := colly.NewCollector(
-		colly.AllowedDomains(baseUrl),
-		colly.CacheDir("/tmp/.cpf_cache"), // comment out during debugging
-	)
-	detailCollector := collector.Clone()
+	app.Usage = `Check Plugin Find - Get metadata from official Check_MK check plugins.
 
-	counter := 0 // ugly but easiest way to implement the scraper
+When executing the app the first time the execution time is in the range of
+approx. 30 seconds. The app has the side effect of caching data in
+/tmp/.cpf_cache for providing the metadata more quickly after the first execution.
+To force getting the data from the website remove this cache file.`
 
-	// Callback triggers on every table line element on the check plugin overview list.
-	// Check plugin specific sites don't contain the check name which
-	// requires to use this parent collector in addition to the childdetailCollector.
-	collector.OnHTML("tr", func(e *colly.HTMLElement) {
-		e.ForEach("td", func(i int, el *colly.HTMLElement) {
-			switch i {
-			case 0:
-				check := Check{
-					Name: el.Text,
+	// main command executed if no -h and -v provided
+	app.Action = func(c *cli.Context) error {
+
+		checks := make([]Check, 0, 1500)
+
+		baseUrl := "mathias-kettner.com"
+		url := "https://mathias-kettner.com/cms_check_plugins_list.html"
+
+		collector := colly.NewCollector(
+			colly.AllowedDomains(baseUrl),
+			colly.CacheDir("/tmp/.cpf_cache"), // comment out during debugging
+		)
+		detailCollector := collector.Clone()
+
+		counter := 0 // ugly but easiest way to implement the scraper
+
+		// Callback triggers on every table line element on the check plugin overview list.
+		// Check plugin specific sites don't contain the check name which
+		// requires to use this parent collector in addition to the childdetailCollector.
+		collector.OnHTML("tr", func(e *colly.HTMLElement) {
+			e.ForEach("td", func(i int, el *colly.HTMLElement) {
+				switch i {
+				case 0:
+					check := Check{
+						Name: el.Text,
+					}
+					checks = append(checks, check)
+				case 1:
+					checks[counter].Title = el.Text
+					el.ForEach("a[href]", func(_ int, ele *colly.HTMLElement) {
+						checks[counter].URL = ele.Request.AbsoluteURL(ele.Attr("href"))
+						// trigger child collector
+						detailCollector.Visit(checks[counter].URL)
+					})
+					counter = counter + 1
 				}
-				checks = append(checks, check)
-			case 1:
-				checks[counter].Title = el.Text
-				el.ForEach("a[href]", func(_ int, ele *colly.HTMLElement) {
-					checks[counter].URL = ele.Request.AbsoluteURL(ele.Attr("href"))
-					// trigger child collector
-					detailCollector.Visit(checks[counter].URL)
-				})
-				counter = counter + 1
-			}
+			})
 		})
-	})
 
-	// Callback triggers on every div element which contains check plugin metadata
-	// on check plugin specific sites.
-	detailCollector.OnHTML("div[class=\"nowiki\"]", func(e *colly.HTMLElement) {
-		// TODO: filter to relevant info and assign correspondingly
-		checks[counter].Description = e.Text
-	})
+		// Callback triggers on every div element which contains check plugin metadata
+		// on check plugin specific sites.
+		detailCollector.OnHTML("div[class=\"nowiki\"]", func(e *colly.HTMLElement) {
+			// TODO: filter to relevant info and assign correspondingly
+			checks[counter].Description = e.Text
+		})
 
-	// debug callbacks
-	collector.OnError(func(_ *colly.Response, err error) {
-		fmt.Println("Error during scraping:", err)
-	})
+		// debug callbacks
+		collector.OnError(func(_ *colly.Response, err error) {
+			fmt.Println("Error during scraping:", err)
+		})
 
-	// for future verbose output
-	// collector.OnRequest(func(r *colly.Request) {
-	// 	fmt.Println("Try to visit:", r.URL.String())
-	// })
+		// for future verbose output
+		// collector.OnRequest(func(r *colly.Request) {
+		// 	fmt.Println("Try to visit:", r.URL.String())
+		// })
 
-	// for future verbose output
-	// collector.OnResponse(func(r *colly.Response) {
-	// 	fmt.Println("Visited:", r.Request.URL)
-	// })
+		// for future verbose output
+		// collector.OnResponse(func(r *colly.Response) {
+		// 	fmt.Println("Visited:", r.Request.URL)
+		// })
 
-	// start scraping
-	collector.Visit(url)
+		// start scraping
+		collector.Visit(url)
 
-	for _, check := range checks {
-		fmt.Println("Name:         " + check.Name)
-		fmt.Println(" Title:       " + check.Title)
-		fmt.Println(" URL:         " + check.URL)
-		fmt.Println(" Description: " + check.Description)
+		for _, check := range checks {
+			fmt.Println("Name:         " + check.Name)
+			fmt.Println(" Title:       " + check.Title)
+			fmt.Println(" URL:         " + check.URL)
+			fmt.Println(" Description: " + check.Description)
+		}
+		return nil
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		fmt.Println(err)
 	}
 }
